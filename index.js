bcbd472903168cdc6dd0da74b24641e0fd580243
prakul1562 <104349313+prakul1562@users.noreply.github.com>
2023-07-26 15:00:04 +0530

    Add files via upload

--- index.js
+++ index.js
@@ -1,85 +1,31 @@
- //detecting button press
- 
- 
- for(var i=0;i<document.querySelectorAll(".drum").length;i++){
+var randomnumber1;
+randomnumber1=Math.floor(6*(Math.random(randomnumber1)))+1;
+//alert(randomnumber1);
+var randomimagesource = "images\\dice" + randomnumber1 +".png";
 
-    document.querySelectorAll(".drum")[i].addEventListener("click",handleClick);
+var image1 = document.querySelectorAll("img")[0];
 
-function handleClick(){
-    var buttonInnerHTML = this.innerHTML;
-    makeSound(buttonInnerHTML);
-    buttonAnimation(buttonInnerHTML);
+image1.setAttribute("src", randomimagesource);
+
+
+var randomnumber2;
+
+randomnumber2=Math.floor(6*(Math.random(randomnumber2)))+1;
+
+var randomimagesource2 = "images\\dice" + randomnumber2 +".png"
+
+var image2 =document.querySelectorAll("img")[1];
+
+image2.setAttribute("src", randomimagesource2);
+
+
+if(randomnumber1>randomnumber2){
+    document.querySelector('h1').innerHTML='Player 1 wins';
 }
+else if(randomnumber1<randomnumber2){
+    document.querySelector('h1').innerHTML='Player 2 wins'
+}
+else{
+    document.querySelector('h1').innerHTML='Draw'
 
- }
-
- //detecting key presses
-
-
- document.addEventListener("keydown",function(event) {
-    makeSound(event.key);
-    buttonAnimation(event.key);
-     
-
- });
-
-
-//function for making sound 
-
- function makeSound(key){
-    switch (key) {
-        case "w":
-          var tom1 =new Audio("sounds/tom-1.mp3");
-          tom1.play(); 
-            break;
-
-        case "a":
-           var tom2 =new Audio("sounds/tom-2.mp3");
-           tom2.play(); 
-           break;
-
-        case "s":
-          var tom3 =new Audio("sounds/tom-3.mp3");
-          tom3.play(); 
-          break; 
-
-        case "d":
-          var tom4 =new Audio("sounds/tom-4.mp3");
-          tom4.play(); 
-          break;
-
-        case "j":
-          var crash =new Audio("sounds/crash.mp3");
-          crash.play(); 
-          break;
-
-        case "k":
-          var kick =new Audio("sounds/kick-bass.mp3");
-          kick.play(); 
-          break;
-
-        case "l":
-          var snare =new Audio("sounds/snare.mp3");
-          snare.play(); 
-          break; 
-
-        default: console.log(buttonInnerHTML);
-            break;
-    }
-
-
- }
-
- function buttonAnimation(currentKey){
-
-
-    var activeButton =document.querySelector("."+ currentKey);
-    activeButton.classList.add("pressed");
-
-    setTimeout(function(){activeButton.classList.remove("pressed");},100);
-
- }
-
-
- 
- 
+}
